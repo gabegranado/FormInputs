1931b7524dc39f530adf72c48a66a44bc514724d
gabegranado <gabe.granado01@gmail.com>
2019-02-13 23:24:39 +0000

    today is febuary/13/19

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,37 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="ggtx01",
+    password="G^a70R0x",
+    hostname="ggtx01.mysql.pythonanywhere-services.com",
+    databasename="ggtx01$postage",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route('/', methods=['GET'])
-def main():
-    # found in ../templates/
-    return render_template("main_page.html")
+db = SQLAlchemy(app)
 
-@app.route('/process_inputs', methods=['POST'])
-def process_inputs():
-    name = request.form.get('input_name', '')
-    dropdown = request.form.get('input_dropdown', '')
-    select = request.form.get('input_select', '')
-    freeform = request.form.get('input_freeform', '')
-    return render_template("main_page.html", input_data=dropdown,
-                           output="You're a wizard %s." % name)
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=Comment.query.all())
+
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+#you need new varibles to get the information in diffrent forms
+#you need to have an empty case
